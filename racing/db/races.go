7aae80a5762ecac62c973025942d81fdb2439b7c
georgep9 <g-patt@outlook.com>
2023-05-08 22:53:26 +1000

    added visible filter to racing protobuf and updated RacesRepo applyFilter func to append visible clause

--- racing/db/races.go
+++ racing/db/races.go
@@ -2,11 +2,12 @@ package db
 
 import (
 	"database/sql"
-	"github.com/golang/protobuf/ptypes"
-	_ "github.com/mattn/go-sqlite3"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	_ "github.com/mattn/go-sqlite3"
 
 	"git.neds.sh/matty/entain/racing/proto/racing"
 )
@@ -77,6 +78,12 @@ func (r *racesRepo) applyFilter(query string, filter *racing.ListRacesRequestFil
 		for _, meetingID := range filter.MeetingIds {
 			args = append(args, meetingID)
 		}
+	}
+
+	if filter.Visible {
+		clauses = append(clauses, "visible=1")
+	} else if !filter.Visible {
+		clauses = append(clauses, "visible=0")
 	}
 
 	if len(clauses) != 0 {
